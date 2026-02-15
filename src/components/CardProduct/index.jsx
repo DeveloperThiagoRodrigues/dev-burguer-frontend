3d69765c1c916e31454d3f802db533fb0383ddd9
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-15 00:25:56 -0300

    Update index.jsx

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -1,26 +1,24 @@
-import PropTypes from 'prop-types';
-import { useCart } from '../../hooks/CartContext';
-import { CardImage, Container } from './styles';
-import { CartButton } from '../CartButton';
-;
+import PropTypes from "prop-types";
+import { CardImage, Container } from "./styles";
+import { CartButton } from "../CartButton";
+import { formatPrice } from "../../utils/formatPrice";
+
+import { useCart } from "../../hooks/CartContext";
 
 export function CardProduct({ product }) {
-    const { putProductInCart } = useCart()
-    return(
-        <Container>
-           <CardImage 
-  src={`${import.meta.env.VITE_BASE_URL}/product-file/${product.path}`} 
-  alt={product.name} 
-/>
-            <div>
-                <p>{product.name}</p>
-                <strong> {product.currencyValue}</strong>
-            </div>
-            <CartButton onClick={() => putProductInCart(product)}></CartButton>
-        </Container>
-    );
+  const { putProductInCart } = useCart();
+  return (
+    <Container>
+      <CardImage src={product.url} alt={product.name} />
+      <div>
+        <p>{product.name}</p>
+        <strong>{formatPrice(product.price)}</strong>
+      </div>
+      <CartButton onClick={() => putProductInCart(product)}></CartButton>
+    </Container>
+  );
 }
 
 CardProduct.propTypes = {
-    product: PropTypes.object,
-}
+  product: PropTypes.object,
+};
