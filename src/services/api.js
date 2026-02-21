3785f379754ae219c97b785372cf51dc86e9a86d
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-21 10:41:33 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
-});
+const apiDevBurger = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001'
+})
 
 
 api.interceptors.request.use( (config) => {
