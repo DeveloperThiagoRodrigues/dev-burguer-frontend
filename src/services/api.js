ac11c46fa44ea9a253a20ce95f08acbb379a4bef
DeveloperThiagoRodrigues <onlinedigitalnegocios@gmail.com>
2026-02-22 15:39:53 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,14 +4,16 @@ export const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-
-api.interceptors.request.use( (config) => {
+api.interceptors.request.use((config) => {
 
     const userData = localStorage.getItem('devburguer:userData');
 
-    const token = userData && JSON.parse(userData).token;
+    if (userData) {
+        const token = JSON.parse(userData).token;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
 
-    config.headers.Authorization = `Bearer ${token}`;
-    
     return config;
-})
+});
